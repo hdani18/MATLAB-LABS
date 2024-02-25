7a34b7f4959276c59753283eeb7cae114c5ead68
hdani18 <daniharshil1800@gmail.com>
2024-02-25 00:50:58 -0500

    Smoothing-filters

---
+++ Smoothing Filters/Box_Filter.m
@@ -0,0 +1,73 @@
+function new_img = Box_Filter() 
+    
+    %%
+    % Linear Smoothing Filter
+    %
+    % Box Filter
+    %
+    % Define Odd Mask Size 3,5,7,....
+    m_size = 3;
+    
+    % Use uigetfile to interactively select an image file
+    [filename, pathname] = uigetfile({'*.jpg;*.png;*.bmp;*.tif', 'Image Files (*.jpg, *.png, *.bmp, *.tif)'}, 'Select an Image File');
+    if filename == 0
+        disp('User canceled the operation.');
+        return;
+    end
+    impath = fullfile(pathname, filename);
+
+    % read image
+    img = imread(impath);
+    
+    % Convert BGR image to GRAY Scale Image (optional)
+    % img = rgb2gray(img);
+    
+    %%
+    % Box Filter Mask
+    %
+    % 1 1 1
+    % 1 1 1
+    % 1 1 1
+    %
+    mask = ones(m_size);
+    
+    % Sum Of All The Elements Of Mask
+    msum = sum(mask, 'all');
+    
+    s = m_size/2-0.5;
+    
+    % Pad The Given Image
+    [nr,nc]=size(img);
+    padded_img = uint8(zeros(nr+2*s,nc+2*s));
+    padded_img((s+1):(nr+s),(s+1):(nc+s)) = img;
+    
+    % Plot Result 
+    subplot(1,3,1),imshow(img);
+    title('Original Gray Image','FontSize',10);
+    subplot(1,3,2),imshow(padded_img);
+    title('Padded Image','FontSize',10);
+    
+    % Convert uint8 to double
+    padded_img=double(padded_img);
+    
+    % Find Size of Padded Image
+    [pnr,pnc]=size(padded_img);
+
+    new_img = zeros(nr,nc);
+    
+    for i = (1+s):(nr+s)
+      for j = (1+s):(nc+s)
+        
+        % Add each value of masked image
+        new_img(i-s,j-s) = sum(sum(mask.*padded_img(i-s:i+s,j-s:j+s)));
+        
+        % Divide result by the sum of mask
+        new_img(i-s,j-s) = new_img(i-s,j-s)/msum;
+        
+      end
+    end
+
+    subplot(1,3,3),imshow(uint8(new_img));
+    title('Box Filter','FontSize',10);
+    
+end
---
+++ Smoothing Filters/Max_Filter.m
@@ -0,0 +1,55 @@
+function new_img = Max_Filter() 
+    
+    %%
+    % Non Linear Smoothing Filter
+    %
+    % Max Filter
+    %
+    % Define Odd Mask Size 3,5,7,....
+    m_size = 3;
+    
+    % Use uigetfile to interactively select an image file
+    [filename, pathname] = uigetfile({'*.jpg;*.png;*.bmp;*.tif', 'Image Files (*.jpg, *.png, *.bmp, *.tif)'}, 'Select an Image File');
+    if filename == 0
+        disp('User canceled the operation.');
+        return;
+    end
+    impath = fullfile(pathname, filename);
+
+    % read image
+    img = imread(impath);
+    
+    % Convert BGR image to GRAY Scale Image (optional)
+    % img = rgb2gray(img);
+    
+    s = m_size/2-0.5;
+    
+    % Pad The Given Image
+    [nr,nc]=size(img);
+    padded_img = uint8(zeros(nr+2*s,nc+2*s));
+    padded_img((s+1):(nr+s),(s+1):(nc+s)) = img;
+    
+    % Plot Result 
+    subplot(1,3,1),imshow(img);
+    title('Original Gray Image','FontSize',10);
+    subplot(1,3,2),imshow(padded_img);
+    title('Padded Image','FontSize',10);
+    
+    % Convert uint8 to double
+    padded_img=double(padded_img);
+    
+    % Find Size of Padded Image
+    [pnr,pnc]=size(padded_img);
+
+    new_img = zeros(nr,nc);
+    
+    for i = (1+s):(nr+s)
+        for j = (1+s):(nc+s)
+            new_img(i-s,j-s) = max(max(padded_img(i-s:i+s,j-s:j+s)));
+        end
+    end
+
+    subplot(1,3,3),imshow(uint8(new_img));
+    title('Max Filter','FontSize',10);
+    
+endfunction
